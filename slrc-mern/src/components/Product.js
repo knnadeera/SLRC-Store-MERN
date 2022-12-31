1a3f3ffa3500be45253d9e4903901b670ec0a92b
Nadeera KN <knnadeera1@gmail.com>
2022-12-31 13:00:30 +0530

    rating component & it's styling added

--- slrc-mern/src/components/Product.js
+++ slrc-mern/src/components/Product.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { Card, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 function Product({ product }) {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`product/${product._id}`}>
-        <Card.Img src={product.image} variant="top"height='50%'/>
+      <a href={`#/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" height="50%" />
       </a>
       <Card.Body>
-        <a href={`product/${product._id}`}>
+        <a href={`#/product/${product._id}`}>
           <Card.Title as="div">
-            <h5>{product.name}</h5>
+            <h6>{product.name}</h6>
           </Card.Title>
         </a>
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
--- slrc-mern/src/screens/HomeScreen.js
+++ slrc-mern/src/screens/HomeScreen.js
@@ -9,8 +9,8 @@ function HomeScreen() {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} key={product._id}/>
+          <Col key={product._id}  sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
