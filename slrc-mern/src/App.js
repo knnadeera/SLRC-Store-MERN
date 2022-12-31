72520e7dbdd288aeec9cfc14adae74feb9ae6b54
Nadeera KN <knnadeera1@gmail.com>
2022-12-31 08:43:51 +0530

    import Bootstrap & create Header, Nav, Footer

--- slrc-mern/src/App.js
+++ slrc-mern/src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>slrc</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
