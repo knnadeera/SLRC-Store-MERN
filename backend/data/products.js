7926b4eb1b65b70a3c4299eb781b00e6da93309c
Nadeera KN <knnadeera1@gmail.com>
2023-01-01 22:12:29 +0530

    bringed the ES Modules in Node.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -77,4 +77,4 @@ const products = [
   },
 ];
 
-module.exports = products;
+export default products;
--- slrc-mern/src/components/Header.js
+++ slrc-mern/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>SLRC Store</Navbar.Brand>
+            <Navbar.Brand><img src='/images/slrclogo.png' width='30'/> SLRC Store</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
