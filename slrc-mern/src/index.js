b266c292481cf9163c6a913d5e975816308c23db
Nadeera KN <knnadeera1@gmail.com>
2023-01-06 13:15:55 +0530

    setting redux to manage app wide state

--- slrc-mern/src/index.js
+++ slrc-mern/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
@@ -8,11 +10,11 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
