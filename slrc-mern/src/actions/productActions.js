9baf8d089b0d18d4d3c16a7a7dd8d598b93c6281
Nadeera KN <knnadeera1@gmail.com>
2023-01-06 19:37:49 +0530

    configured the product actions & bringed the data thrue redux to product screan

--- slrc-mern/src/actions/productActions.js
+++ slrc-mern/src/actions/productActions.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import {
-  PRODUCTS_LIST_FAIL,
+  PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -14,7 +17,25 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
-      type: PRODUCTS_LIST_FAIL,
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
