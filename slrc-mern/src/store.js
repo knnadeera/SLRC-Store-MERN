149eef973dcbda4242703f507fbd18dbf20e888d
Nadeera KN <knnadeera1@gmail.com>
2023-01-06 13:20:02 +0530

    added product list reducer & global constants

--- slrc-mern/src/store.js
+++ slrc-mern/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from './reducers/productReducers';
+import { productListReducer } from '../reducers/productReducers';
 
 const reducer = combineReducers({
   productList:productListReducer,
