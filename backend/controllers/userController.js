77e35930956502cd8ac77d01887b0d62055af7c4
Nadeera KN <knnadeera1@gmail.com>
2023-01-09 13:55:25 +0530

    setting backend user registration & password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,12 +25,45 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc register new user
+//@rout POST /api/users
+//@access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // @desc Get User profile
 //@rout GET /api/users/profile
 //@access Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -44,8 +77,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("User not found");
   }
-
-  
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
