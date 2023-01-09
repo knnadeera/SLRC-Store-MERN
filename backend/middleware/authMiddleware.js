6e4ef54f1843a2c7fb964856c065940f3334023f
Nadeera KN <knnadeera1@gmail.com>
2023-01-09 12:57:34 +0530

    getting authentication & user data with custom authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      console.log(decoded)
+      req.user = await User.findById(decoded.id).select("-password");
+
+    } catch (error) {
+        console.error(error)
+        res.status(401)
+        throw new Error('not auth')
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("no token");
+  }
+
+  next();
+});
